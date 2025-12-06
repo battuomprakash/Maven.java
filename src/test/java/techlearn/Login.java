0c87374bb3819495e2e2eb47a266ea8e8148ac81
battuomprakash <batuomprakash@gmail.com>
2025-12-06 06:19:26 +0530

    modified again

--- src/test/java/techlearn/Login.java
+++ src/test/java/techlearn/Login.java
@@ -1,6 +1,7 @@
 package techlearn;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
@@ -16,7 +17,7 @@ public class Login {
   @Test
   public void Google() {
 	  driver.get("https://www.google.com");
-	  driver.findElements(By.name("q")).sendKeys("hello");
+	  driver.findElement(By.name("q")).sendKeys("hello");
 	  
   }
   
